ada7c05987a648488e1d711f629bdc95f649e3f5
anison99_ <anita.zych9941@gmail.com>
2023-11-30 18:37:15 +0100

    add new calsses

---
+++ tijoLibrary/src/main/java/com/library/tijoLibrary/models/Category.java
@@ -0,0 +1,10 @@
+package com.library.tijoLibrary.models;
+
+public class Category {
+    public Category(String name) {
+    }
+
+    public boolean containsBook(Book categorizedBook) {
+        return false;
+    }
+}
---
+++ tijoLibrary/src/main/java/com/library/tijoLibrary/models/Library.java
@@ -0,0 +1,6 @@
+package com.library.tijoLibrary.models;
+
+public class Library {
+    public static void addCategory(Category category) {
+    }
+}
