76bfedbff65ae3e5cb303e45c0c0e9fdea65ec99
anison99_ <anita.zych9941@gmail.com>
2023-12-12 17:53:59 +0100

    add new calsses

---
+++ tijoLibrary/src/main/java/com/library/tijoLibrary/models/BookStatuses.java
@@ -0,0 +1,51 @@
+package com.library.tijoLibrary.models;
+
+import javax.persistence.*;
+
+@Entity
+public class BookStatuses {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "book_id", nullable = false)
+    private Long bookId;
+
+    @Column(name = "status", nullable = false)
+    private Boolean status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(Long bookId) {
+        this.bookId = bookId;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+}
---
+++ tijoLibrary/src/main/resources/static/scripts/navbar.js
@@ -0,0 +1,8 @@
+const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+const navbarCollapse = document.querySelector('.navbar-collapse');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navbarCollapse.classList.remove('show');
+  });
+});
