c6b723808c947512feed8f5fd49a5be4fe021c52
anison99_ <anita.zych9941@gmail.com>
2024-01-06 14:38:56 +0100

    add swagger

--- tijoLibrary/src/main/java/com/library/tijoLibrary/TijoLibraryApplication.java
+++ tijoLibrary/src/main/java/com/library/tijoLibrary/TijoLibraryApplication.java
@@ -3,6 +3,7 @@ package com.library.tijoLibrary;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication(scanBasePackages = "com.library.tijoLibrary")
 public class TijoLibraryApplication {
 
--- tijoLibrary/src/main/java/com/library/tijoLibrary/WebConfig.java
+++ tijoLibrary/src/main/java/com/library/tijoLibrary/WebConfig.java
@@ -1,8 +1,10 @@
 package com.library.tijoLibrary;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -11,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedOrigins("http://localhost:1002");
+                .allowedOrigins("http://localhost:5000");
     }
 }
